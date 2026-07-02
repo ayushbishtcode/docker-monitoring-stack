ec14a639196596e42d6ae5ebc58fc8cf835a6c23
ayushbishtcode <ayushbisht044@gmail.com>
2026-07-02 17:24:53 +0530

    Initial monitoring stack with Prometheus, Grafana and Node Exporter

---
+++ app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+
+app = Flask(__name__)
+
+# Metric
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total HTTP Requests"
+)
+
+@app.route("/")
+def home():
+
+    REQUEST_COUNT.inc()
+
+    return {
+        "message": "Monitoring Stack Project",
+        "status": "running"
+    }
+
+@app.route("/metrics")
+def metrics():
+
+    return generate_latest(), 200, {
+        "Content-Type": CONTENT_TYPE_LATEST
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5004)
